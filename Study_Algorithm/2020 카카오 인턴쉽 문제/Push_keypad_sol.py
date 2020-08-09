1e174b8e2c4b0c0bd52d0476717eedb8f07eb443
DonghakPark <donghark03@naver.com>
2020-08-09 20:06:08 +0900

    Create Push_keypad_sol.py

---
+++ Study_Algorithm/2020 카카오 인턴쉽 문제/Push_keypad_sol.py
@@ -0,0 +1,11 @@
+def solution(numbers, hand):
+    answer =''
+    for number in numbers:
+        
+
+    return answer
+
+if __name__=="__main__":
+    numbers1 = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+    hand = "right"
+    print(solution(numbers1, hand))
