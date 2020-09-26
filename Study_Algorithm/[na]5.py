8b126021e704c0110fd5418c4b2a7ca5cfd6117e
DonghakPark <donghark03@naver.com>
2020-09-26 19:13:29 +0900

    0926

--- Study_Algorithm/[na]5.py
+++ Study_Algorithm/[na]5.py
@@ -1,5 +1,5 @@
 n,m = map(int, input().split())
-d = [[0] + m for _ in range(n)]
+d = [[0] * m for _ in range(n)]
 
 x,y, direction = map(int, input().split())
 d[x][y] = 1
@@ -17,3 +17,36 @@ def turn_left():
     direction -= 1
     if direction == -1:
         direction = 3
+
+#시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    #왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    #회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] ==0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+print(count)
