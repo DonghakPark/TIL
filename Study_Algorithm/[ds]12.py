1c12bbd5334fb1efa47559a86f3e5e1c6f939119
DonghakPark <donghark03@naver.com>
2020-10-13 09:55:27 +0900

    Update [ds]12.py

--- Study_Algorithm/[ds]12.py
+++ Study_Algorithm/[ds]12.py
@@ -1,1 +1,17 @@
 # 시험 감독 문제
+import math
+
+N = int(input())
+
+A = (list(map(int, input().split())))
+B,C = map(int, input().split())
+
+total = 0
+
+for i in range(N):
+    if A[i] <= B:
+        total += 1
+    else:
+        total += math.ceil((A[i] - B)/C) + 1
+
+print(total)
