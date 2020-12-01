da0157305f86469d351dabf6dc52c0b4a5540158
DonghakPark <donghark03@naver.com>
2020-12-01 09:46:34 +0900

    20201201

---
+++ ch02_java/src/Ex2_1.java
@@ -0,0 +1,7 @@
+public class Ex2_1 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println(3+5);
+    }
+}
---
+++ ch02_java/src/Ex2_2.java
@@ -0,0 +1,11 @@
+public class Ex2_2 {
+    public static void main(String[] args){
+        int a = 14;
+        int b = 25;
+        System.out.println(a + b);
+        System.out.println(a / b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+
+    }
+}
