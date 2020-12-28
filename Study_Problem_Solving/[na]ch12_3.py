20e166e81af756a5458dcff79b0bc1a188708638
DonghakPark <donghark03@naver.com>
2020-12-28 10:31:10 +0900

    Update [na]ch12_3.py

--- Study_Problem_Solving/[na]ch12_3.py
+++ Study_Problem_Solving/[na]ch12_3.py
@@ -5,10 +5,26 @@ contact : donghark03@naver.com
 """
 
 def solution(s):
-    answer = 0
-    
-    return answer
+    answer = len(s)
 
+    for step in range(1, len(s)//2 + 1):
+        compressed = ''
+        prev = s[0:step]
+        count = 1
+
+        for j in range(step, len(s), step):
+            if prev == s[j:j+step]:
+                count += 1
+
+            else:
+                compressed += str(count) + prev if count >= 2 else prev
+                prev = s[j:j+step]
+                count =1
+        compressed += str(count) + prev if count >= 2 else prev
+
+        answer = min(answer, len(compressed))
+
+    return answer
 
 if __name__ == "__main__":
     t1 = "aabbaccc"
