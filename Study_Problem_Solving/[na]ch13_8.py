24da8ccce96afaed309ffe4bd4aa29538744d982
DonghakPark <donghark03@naver.com>
2021-01-05 04:58:23 +0900

    [Add] Problem Solving

--- Study_Problem_Solving/[na]ch13_8.py
+++ Study_Problem_Solving/[na]ch13_8.py
@@ -2,5 +2,6 @@
 블록 이동하기 문제
 author : donghak pakr
 contact : donghark03@naver.com
+TODO : 나중에 풀 것
 """
 
--- Study_Problem_Solving/[na]ch15_2.py
+++ Study_Problem_Solving/[na]ch15_2.py
@@ -2,4 +2,5 @@
 고정점 찾기 문제
 author : donghak park
 contact : donghark03@naver.com
+TODO : 나중에 풀 것
 """
