99ad91397048514821884170ebbe755075656ab8
DonghakPark <donghark03@naver.com>
2021-01-24 11:21:07 +0900

    Update [boj]2407.py

--- Study_Problem_Solving/[boj]2407.py
+++ Study_Problem_Solving/[boj]2407.py
@@ -4,3 +4,13 @@ author : donghak park
 contact: donghark03@naver.com
 """
 
+import math
+
+N, M = map(int, input().split())
+
+X = math.factorial(N)
+Y = (math.factorial(N-M)) * (math.factorial(M))
+
+answer = X//Y
+
+print(answer)
