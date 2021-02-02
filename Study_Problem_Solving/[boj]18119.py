d036d303f39a1b18f53434e9e8600d85313c5faf
DonghakPark <donghark03@naver.com>
2021-02-02 20:18:24 +0900

    [Add] 2021-02-02

--- Study_Problem_Solving/[boj]18119.py
+++ Study_Problem_Solving/[boj]18119.py
@@ -3,31 +3,6 @@
 author : donghak park
 contact: donghark03@naver.com
 """
-
-#
-# def search(x, y, direct):
-#     global answer
-#
-#     if x == N-1 and y == N-1:
-#         answer += 1
-#         return
-#
-#     if direct == 0 or direct == 1 or direct == 2:
-#         if x + 1 < N and y + 1 < N:
-#             if arr[x + 1][y] == 0 and arr[x + 1][y + 1] == 0 and arr[x][y + 1] == 0:
-#                 search(x+1, y+1, 2)
-#
-#     if direct == 0 or direct == 2:
-#         if y + 1 < N:
-#             if arr[x][y + 1] == 0:
-#                 search(x, y+1, 0)
-#
-#     if direct == 1 or direct == 2:
-#         if x + 1 < N:
-#             if arr[x + 1][y] == 0:
-#                 search(x+1, y, 1)
-#
-
 import sys
 input = sys.stdin.readline
 
@@ -51,3 +26,29 @@ for r in range(1, N):
             dp[2][r][c] = dp[2][r-1][c] + dp[1][r-1][c]
 
 print(dp[0][N-1][N-1] + dp[1][N-1][N-1] + dp[2][N-1][N-1])
+
+
+"""
+또 다른 풀이 ( 시간 초과남 )
+def search(x, y, direct):
+    global answer
+
+    if x == N-1 and y == N-1:
+        answer += 1
+        return
+
+    if direct == 0 or direct == 1 or direct == 2:
+        if x + 1 < N and y + 1 < N:
+            if arr[x + 1][y] == 0 and arr[x + 1][y + 1] == 0 and arr[x][y + 1] == 0:
+                search(x+1, y+1, 2)
+
+    if direct == 0 or direct == 2:
+        if y + 1 < N:
+            if arr[x][y + 1] == 0:
+                search(x, y+1, 0)
+
+    if direct == 1 or direct == 2:
+        if x + 1 < N:
+            if arr[x + 1][y] == 0:
+                search(x+1, y, 1)
+"""
