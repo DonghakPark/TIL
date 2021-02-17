d1d5294bbe0c82cee2eb0d1b73e1681e6e3756cb
donghakpark <donghark03@naver.com>
2021-02-18 02:46:07 +0900

    Delete Ex2_1.java

--- Study_Language/JAVA/src/JAVA_Practice/Ex2_1.java
+++
@@ -1,9 +0,0 @@
-package JAVA_Practice;
-
-public class Ex2_1 {
-    public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println(3+5);
-    }
-}
