d649325e0e6e06416b6a08ef145fbc5ff8fa3142
donghakpark <donghark03@naver.com>
2021-02-21 02:38:05 +0900

    Update Directory

---
+++ Study_Language/JAVA/src/JAVA_Practice/SimpleExample/EX2_3.java
@@ -0,0 +1,16 @@
+package JAVA_Practice.SimpleExample;
+
+public class EX2_3 {
+    public static void main(String[] args){
+        int score =100;
+        final int score2 = 200;
+
+        final int score3;
+        score3 = 1000;
+        //초기화랑 선언 따로 할 수 있음
+
+        System.out.println(score);
+        System.out.println(score2);
+//        score2 += 1;
+    }
+}
---
+++ Study_Language/JAVA/src/JAVA_Practice/SimpleExample/Ex4_7.java
@@ -0,0 +1,15 @@
+package JAVA_Practice.SimpleExample;
+
+public class Ex4_7 {
+    public static void main(String args[]){
+        int num = 0;
+
+        for ( int i = 1; i <= 20; i ++)
+        {
+            System.out.println(Math.random());
+        }
+        for ( int i =0; i<=20; i++){
+            System.out.println((int)(Math.random()*10+1));
+        }
+    }
+}
