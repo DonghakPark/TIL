3d1878b7a5c9ec8dddc5b204c7736a668813b1ac
donghakpark <donghark03@naver.com>
2021-03-11 00:09:35 +0900

    Update [boj]2579.py

--- Study_Problem_Solving/[boj]2579.py
+++ Study_Problem_Solving/[boj]2579.py
@@ -15,6 +15,7 @@ for i in range(N):
 dp[0] = arr[0]
 dp[1] = arr[0] + arr[1]
 dp[2] = max(arr[1] + arr[2], arr[0] + arr[2])
+
 for i in range(3, N):
     dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i])
 
