16975c754f017b0cdd1904c5b232f9defb90af9a
donghakpark <donghark03@naver.com>
2021-03-27 01:03:12 +0900

    Update [boj]9205.py

--- Study_Problem_Solving/[boj]9205.py
+++ Study_Problem_Solving/[boj]9205.py
@@ -19,7 +19,6 @@ for test_case in range(T):
     for i in range(N+2):
         for j in range(N+2):
             if i != j:
-
                 diff = abs(arr[i][0] - arr[j][0]) + abs(arr[i][1] - arr[j][1])
                 if diff <= 1000:
                     possible[i][j] = 1
@@ -28,6 +27,7 @@ for test_case in range(T):
         for a in range(N+2):
             for b in range(N+2):
                 possible[a][b] = min(possible[a][b], possible[a][k] + possible[k][b])
+
     if possible[0][N+1] == int(1e9):
         print("sad")
     else:
