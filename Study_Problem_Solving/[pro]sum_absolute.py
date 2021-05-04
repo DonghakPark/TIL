d30e8e4c4e17ac9b796ebb6cb28b3bee53fe7a2a
donghakpark <donghark03@naver.com>
2021-05-05 00:11:35 +0900

    [Add] 2021-05-05

---
+++ Study_Problem_Solving/[pro]sum_absolute.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for num, sign in zip(absolutes, signs):
+        if sign:
+            answer += num
+        else:
+            answer -= num
+
+    return answer
